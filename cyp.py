7381cc048946a270d1cb92854271ac22e242fef9
Onirique <76112475+junvalentine@users.noreply.github.com>
2022-08-10 22:36:24 +0700

    Update cyp.py

--- cyp.py
+++ cyp.py
@@ -1,18 +1,5 @@
 from pwn import *
-import sys
-import math
-import numpy
-from Crypto.PublicKey import RSA
-from Crypto.Util.number import bytes_to_long, long_to_bytes,getPrime, inverse
-from Crypto.Util import number
-from Crypto.Cipher import AES
-from hashlib import sha256
-from gmpy2 import *
-import owiener
-from random import getrandbits
-from Crypto.Util.Padding import pad,unpad
-import binascii
-from egcd import egcd
+from Crypto.Util.number import inverse
 
 conn = remote("04.cr.yp.toc.tf", 13777)
 conn.sendlineafter(b"[Q]uit", b"G")
@@ -20,12 +7,11 @@ inp = conn.recvlinesS(4)
 q = int(inp[1].split("= ")[1])
 r = int(inp[2].split("= ")[1])
 s = int(inp[3].split("= ")[1])
-print(q,r,s)
+
 conn.sendlineafter(b"[Q]uit", b"S")
 ques = conn.recvlineS()
 ques = conn.recvlineS()
-l="| please send requested solution like x, y such that x is 14"
-print(len(ques),ques,len(l))
+
 i=0
 while i<5 :
     k=int(ques[58:60])
@@ -44,4 +30,5 @@ while i<5 :
         out=conn.recvlinesS(2)
     print(out)
     ques = out[1]
-
+  
+#Congrats, you got the flag: CCTF{f1nDin9_In7Eg3R_50Lut1Ons_iZ_in73rEStIn9!}
